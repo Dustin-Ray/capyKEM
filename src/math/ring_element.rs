6e6bf44c644767f30ed9940abd006d74e73027dc
Dr. Capybara <dustinray313@gmail.com>
2024-03-17 19:51:44 -0700

    deps

--- src/math/ring_element.rs
+++ src/math/ring_element.rs
@@ -11,10 +11,6 @@ use sha3::{
     Shake256,
 };
 
-use hybrid_array::Array;
-use hybrid_array::ArrayOps;
-use typenum::{Unsigned, U0, U6};
-
 /// A polynomial is an element of the ring R. It is an array of 256 coefficients
 /// which themselves are [F].
 #[derive(Clone, Copy)]
